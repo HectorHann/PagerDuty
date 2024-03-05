4c9a0f7160f74be8a7ebb11c4eeb3f11e29914f0
Hector Han <hector@nexttrucking.com>
2024-03-05 10:06:29 +0800

    add the use seat updates

---
+++ Constant.py
@@ -0,0 +1,18 @@
+# Replace with US engineer PagerDuty user id
+US_ONCALL = {"PLDL2XZ", "PW720LE", "P7QVCB2", "P47X15V", "PSXE09R", "PZS4YUO", "P4H9RZO", "PZO34BO", "P4OYULV"}
+# Replace with your actual PagerDuty API token
+API_TOKEN = 'u+mg44nvL2apshyzRo7w'
+# Replace with the actual service ID where the incidents should be created
+SERVICE_ID = 'P8CDHXU'
+# Replace with the actual incident message
+INCIDENT_TITLE = '2024Q1 PagerDuty Test For '
+
+XIAN_ONCALL = [
+    "Leo Huang", "Andrew Zhang", "Evan Wang", "Hector Han", "Joy Mu",
+    "Max Ma", "Ori Zhai", "Ryan Zheng", "Eric Tan", "Karl Shi",
+    "Luke Wang", "Mike Guo", "Rey Wang", "Richard Li", "Will Wang",
+    "Kris Qiu", "Michael Li", "Seven Luo", "Barry Li", "Billy Han",
+    "Halvin Shen", "Lipper Li", "Nolan Shen", "Noodles Wang", "Robby Zhao",
+    "Luka Shi", "River Zeng", "Yao Dang", "Sam", "Ian Wang",
+    "Leon Liu", "Norman Else", "Illu Ying"
+]
